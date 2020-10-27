495f5953e8ada936ceeb8448f502b2dbbedf0224
Madhavi Sattiraju <52825067+MadhaviSattiraju@users.noreply.github.com>
2020-10-27 23:56:18 +0530

    sum of digits

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+ int main()
+ {
+ 	int x;
+ 	int y;
+ 	int sum;
+ 	
+ 	
+ 	printf("Enter numbers");
+ 	scanf("%d%d",&x,&y);
+ 	sum=x+y;
+ 	
+ 	printf("sum=%d",sum);
+ 	getch();
+    return 0;
+ }
